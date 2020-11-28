1c6c33916a8c0567b206922289ca183c9c98c1db
KoalaV2 <theo.johanson@gmail.com>
2020-11-28 17:22:18 +0100

    added bunch of stuff

--- main.py
+++ main.py
@@ -2,23 +2,41 @@
 
 import bcrypt
 import sqlite3
-conn = sqlite3.connect('users.db')
+conn = sqlite3.connect('database.db')
 c = conn.cursor()
 def writepasswd():
     c.execute('''CREATE TABLE IF NOT EXISTS users (username,password)''')
-    username = input("What is your username? \n :").encode("utf-8")
+    username = input("What is your username? \n :") 
     passwd = input("What is your password? \n :").encode("utf-8")
     hashed = bcrypt.hashpw(passwd, bcrypt.gensalt())
     c.execute("INSERT INTO users VALUES (?,?);",(username,hashed))
     conn.commit()
     conn.close()
     return passwd,hashed
+def checkpasswd():
+#    cursor= c.execute("SELECT username,password FROM users")
+    # for row in cursor:
+        # print("USERNAME = ", row[0])
+        # print("HASHEDPASSWD = ", row[1]) 
+# #    if bcrypt.checkpw(passwd,hashed):
+# #        print("It matches!")
+    conn.close()
+def deleteuser():
+    cursor= c.execute("SELECT username,password FROM users")
+    for row in cursor:
+        print("\n USERNAME = ", row[0])
+    user = input("\n What user do you want to delete? \n : ")
+    c.execute("DELETE from users where username =" + f'"{user}"' + ";")
+    conn.commit()
 def main():
-    passwd, hashed = writepasswd()
-    if bcrypt.checkpw(passwd, hashed):
-        print("It matches!")
-
+    choice = input("signup, delete or login? \n :")
+    if choice == "signup":
+        writepasswd()
+    elif choice == "login":
+        checkpasswd()
+    elif choice == "delete":
+        deleteuser()
     else:
-        print("Password does not match")
+        print("Something went wrong")
 if __name__ == "__main__":
     main()
